f9fbbb4173f954b85589f9538bfb860512707092
Olia1011 <olgamelnyk10111989@gmail.com>
2020-05-04 18:57:42 +0300

    Updates

---
+++ precache-manifest.08ff138bebcb25524d2440fc711d877a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9dc5c6d5b5088ed54435181733e5ebc",
+    "url": "/goit-react-homework-02-phonebook/index.html"
+  },
+  {
+    "revision": "2fb1df07e93b49e0335a",
+    "url": "/goit-react-homework-02-phonebook/static/css/main.4b1287b3.chunk.css"
+  },
+  {
+    "revision": "3cd276cb628ea4caaa50",
+    "url": "/goit-react-homework-02-phonebook/static/js/2.28c14572.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-homework-02-phonebook/static/js/2.28c14572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fb1df07e93b49e0335a",
+    "url": "/goit-react-homework-02-phonebook/static/js/main.cc9b7cfe.chunk.js"
+  },
+  {
+    "revision": "c0e82895b0fcc02b6f3f",
+    "url": "/goit-react-homework-02-phonebook/static/js/runtime-main.21379792.js"
+  }
+]);
